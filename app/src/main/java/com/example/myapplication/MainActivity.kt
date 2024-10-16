e6985440933ce1bfbf079554d9c3b2e7b5df800b
defen <defender228822@gmail.com>
2024-10-16 18:00:27 +0300

    Лаба 5 (Бирюков)

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.myapplication
+package com.example.mobiledeveloping
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -10,37 +10,47 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.myapplication.ui.theme.MyApplicationTheme
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.mobiledeveloping.ui.theme.MobileDevelopingTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MyApplicationTheme {
-                // A surface container using the 'background' color from the theme
+            MobileDevelopingTheme {
+
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+
+                    AppNavigation()
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
+
+
+
 @Composable
-fun GreetingPreview() {
-    MyApplicationTheme {
-        Greeting("Android")
+internal fun AppNavigation() {
+
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = Screens.MainScreen.screenName) {
+        composable(Screens.MainScreen.screenName) { MainScreen(navController = navController) }
+        composable(
+            "${Screens.ChatScreen.screenName}/{chatIndex}",
+            arguments = listOf(navArgument("chatIndex") { type = NavType.IntType })
+        ) { backStackEntry ->
+            val chatIndex = backStackEntry.arguments?.getInt("chatIndex") ?: 0
+            ChatScreen(navController = navController, chatIndex = chatIndex)
+        }
     }
 }
