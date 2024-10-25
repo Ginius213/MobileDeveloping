6b851156798961e131034aa31df530e35afe7588
Birukov <defender228822@gmail.com>
2024-10-25 19:07:47 +0300

    Прогноз погоды 9. Полное интегрирование API в карточки

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -33,7 +33,20 @@ class MainActivity : ComponentActivity() {
                 val daysList = remember {
                     mutableStateOf(listOf<WeatherModel>())
                 }
-                getData("London", this, daysList)
+                val currentDay = remember {
+                    mutableStateOf(WeatherModel(
+                    "",
+                    "",
+                    "0.0",
+                    "",
+                    "",
+                    "0.0",
+                    "0.0",
+                    "",
+                    )
+                    )
+                }
+                getData("London", this, daysList, currentDay)
                 Image(
                     painter = painterResource(id = R.drawable.background_im),
                     contentDescription = "im1",
@@ -43,7 +56,7 @@ class MainActivity : ComponentActivity() {
                     contentScale = ContentScale.FillHeight
                 )
                 Column {
-                    MainCard()
+                    MainCard(currentDay)
                     TabLayout(daysList)
                 }
             }
@@ -52,7 +65,9 @@ class MainActivity : ComponentActivity() {
 }
 
 
-private fun getData(city: String, context: Context, daysList: MutableState<List<WeatherModel>>){
+private fun getData(city: String, context: Context,
+                    daysList: MutableState<List<WeatherModel>>,
+                    currentDay: MutableState<WeatherModel>){
     val url = "https://api.weatherapi.com/v1/forecast.json?key=$API_KEY" +
             "&q=$city"+
             "&days=" +
@@ -65,6 +80,7 @@ private fun getData(city: String, context: Context, daysList: MutableState<List<
         {
             response ->
             val list = getWeatherByDays(response)
+            currentDay.value = list[0]
             daysList.value = list
         },
         {
