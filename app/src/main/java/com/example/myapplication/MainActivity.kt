adcebc4248c69e980cb6bf50fbc922cbbae590ca
Birukov <defender228822@gmail.com>
2024-11-05 22:48:42 +0300

    Справочник ботаника 5. Использование db и новый библиотек

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,34 +1,19 @@
 package com.example.myapplication
 
-import android.annotation.SuppressLint
-import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material.Scaffold
-import androidx.compose.material.rememberScaffoldState
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.myapplication.ui.theme.MyApplicationTheme
-import com.example.myapplication.ui_components.DrawerMenu
 import com.example.myapplication.ui_components.InfoScreen
-import com.example.myapplication.ui_components.MainListItem
 import com.example.myapplication.ui_components.MainScreen
-import com.example.myapplication.ui_components.MainTopBar
-import com.example.myapplication.utils.DrawerEvents
-import com.example.myapplication.utils.IdArrayList
 import com.example.myapplication.utils.ListItem
 import com.example.myapplication.utils.Routes
-import kotlinx.coroutines.launch
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -41,7 +26,7 @@ class MainActivity : ComponentActivity() {
                     startDestination = Routes.MAIN_SCREEN
                 ){
                     composable(Routes.MAIN_SCREEN){
-                        MainScreen(context = this@MainActivity){ listItem ->
+                        MainScreen{ listItem ->
                             item = listItem
                             navController.navigate(Routes.INFO_SCREEN)
                         }
